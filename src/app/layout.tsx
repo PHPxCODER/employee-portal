d8d29655cfae54b88fc4ca7d0b9d8c1971160b96
Subhadeep <22051558@kiit.ac.in>
2025-08-15 22:01:12 +0530

    Add SessionWrapper for NextAuth Session 🙄
    Introduced a SessionWrapper component using next-auth's SessionProvider to wrap the app layout, enabling session context throughout the application. Updated env.example with clearer LDAP configuration instructions and improved variable naming.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import SessionWrapper from "@/components/SessionWrapper";
 import { ThemeProvider } from "@/components/theme-provider"
 import "./globals.css";
 
@@ -27,7 +28,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      ><SessionWrapper>
         <ThemeProvider
             attribute="class"
             defaultTheme="system"
@@ -35,6 +36,7 @@ export default function RootLayout({
           >
             {children}
         </ThemeProvider>
+      </SessionWrapper>
       </body>
     </html>
   );
